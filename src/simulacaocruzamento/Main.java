12fdc90295b95b89450d82a5746df445c4ccf909
Alface <pedrocesar1410@gmail.com>
2019-04-25 21:40:51 -0300

    Remover linha desnecessária

--- src/simulacaocruzamento/Main.java
+++ src/simulacaocruzamento/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         JFrame f = new JFrame("Simulação de cruzamento");        
         Carro[] carros = new Carro[20];
-        GUI ui = new GUI(carros, Carro.getSemaforos());
+        GUI gui = new GUI(carros, Carro.getSemaforos());
         
         Timer timer = new Timer();        
         TimerTask task = new TimerTask() {
@@ -24,7 +24,6 @@ public class Main {
                         carros[i] = new Carro();
                         b = false;
                     }
-                    ui.setCarros(carros);
                 }
             }
         };
@@ -32,7 +31,8 @@ public class Main {
         timer.schedule(task, 1000L, 1000L);
         
         f.setSize(800, 800);
-        f.add(ui);
+        f.setLocationRelativeTo(null);
+        f.add(gui);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setVisible(true);
     }
