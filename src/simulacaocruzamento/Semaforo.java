c8593eed37913f6fd3665c031b358c1f98cb51ca
Alface <pedrocesar1410@gmail.com>
2019-04-25 21:40:14 -0300

    Adicionar estado amarelo

--- src/simulacaocruzamento/Semaforo.java
+++ src/simulacaocruzamento/Semaforo.java
@@ -1,64 +1,67 @@
 package simulacaocruzamento;
 
+import java.awt.Color;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Semaforo {
-    private boolean estado;
-    private int x, y, sentido, carrosParados;
-    public static final boolean FECHADO = false, ABERTO = true;   
+    private Color cor;
+    private int x, y, sentido, carrosParados;  
     public static final int COMPRIMENTO = 20, LARGURA = 12;
     public static final int 
             SENTIDO_DIREITA  = 0,
             SENTIDO_CIMA     = 1,
             SENTIDO_ESQUERDA = 2,
             SENTIDO_BAIXO    = 3;
-    
-    
+
     public Semaforo(int sentido) {
         this(sentido, true);
     }
     
-    public Semaforo(int sentido, boolean estado) {
-        this.estado = estado;
+    public Semaforo(int sentido, boolean aberto) {
+        this.cor = aberto ? Color.green : Color.red;
         this.sentido = sentido;
         x = (sentido == 0 || sentido == 3 ? 350 : 450) + (sentido % 2 == 0 ? LARGURA / 2 : COMPRIMENTO / 4) * (sentido < 2 ? -1 : 1);
         y = (sentido > 1 ? 350 : 450) + (sentido % 2 == 1 ? LARGURA / 2 : COMPRIMENTO / 4) * (sentido == 0 || sentido == 3 ? -1 : 1);
          
-        Timer timerAbre = new Timer();        
-        TimerTask abre = new TimerTask() {
+        Timer timerVerde = new Timer();        
+        TimerTask taskVerde = new TimerTask() {
             @Override
             public void run() {
-                setEstado(Semaforo.ABERTO);
+                setCor(Color.green);
                 carrosParados = 0;
             }
         };
         
-        Timer timerFecha = new Timer();
-        TimerTask fecha = new TimerTask() {
+        Timer timerAmarelo = new Timer();
+        TimerTask taskAmarelo = new TimerTask() {
             @Override
             public void run() {
-                setEstado(Semaforo.FECHADO);
+                setCor(Color.yellow);
             }
         };
         
-        if(estado == Semaforo.ABERTO) {
-            timerAbre.schedule(abre, 11000L, 10000L);
-            timerFecha.schedule(fecha, 5000L, 10000L);
-        } else {
-            timerAbre.schedule(abre, 6000L, 10000L);
-            timerFecha.schedule(fecha, 10000L, 10000L);
-        }
+        Timer timerVermelho = new Timer();
+        TimerTask taskVermelho = new TimerTask() {
+            @Override
+            public void run() {
+                setCor(Color.red);
+            }
+        };
+        
+        timerVerde.schedule(taskVerde, aberto ? 0L : 5000L, 10000L);
+        timerAmarelo.schedule(taskAmarelo, aberto ? 4000L : 9000L, 10000L);
+        timerVermelho.schedule(taskVermelho, aberto ? 5000L : 0L, 10000L);
         
         carrosParados = 0;
     }
     
-    public boolean getEstado() {
-        return this.estado;
+    public Color getCor() {
+        return this.cor;
     }
     
-    public void setEstado(boolean estado) {
-        this.estado = estado;
+    public void setCor(Color cor) {
+        this.cor = cor;
     }
     
     public int getX() {
